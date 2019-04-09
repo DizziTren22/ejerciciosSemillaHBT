974cded8463c12a1a45be6f5c422cbb53d8024d2
DizziTren22 <dizzitren@gmail.com>
2019-04-09 18:11:17 -0500

    commit angular

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angularSesion2/src/app/nucleo/capa/principal/creacion-personas/creacion-personas.component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angularSesion2/src/app/nucleo/capa/principal/creacion-personas/creacion-personas.component.ts
@@ -7,15 +7,18 @@ import { TipoIdentificacion } from '../apoyo/modelo/tipoIdentificacion';
   templateUrl: './creacion-personas.component.html'
 })
 export class CreacionPersonasComponent implements OnInit {
-  
-  public header : string = 'Crear personas';
+
+  public header: string = 'Crear personas';
   public persona: PersonaDTO;
-  public tipoIdentificacion : TipoIdentificacion;
+  // public tipoIdentificacion : TipoIdentificacion;
   public personas: PersonaDTO[];
+  datos;
+  public opcionSeleccionado: string = '0'; // Iniciamos
+  public verSeleccion: string = '';
   constructor() { }
 
   ngOnInit() {
-   
+
     this.persona = {
       id: '0',
       nombre: '',
@@ -23,11 +26,26 @@ export class CreacionPersonasComponent implements OnInit {
       tipoIdentificacion: '',
       numeroIdentificacion: 0,
       mayorEdad: false,
-      numeroTelefonico : '',
+      numeroTelefonico: '',
       sexo: ''
     },
-      this.personas = [];
-      this.tipoIdentificacion = new TipoIdentificacion();
+
+    this.personas = [];
+    this.datos= ["Cedula", "Tarjeta Identidad"];
   }
+  capturar() {
+    // Pasamos el valor seleccionado a la variable verSeleccion
+    this.verSeleccion = this.opcionSeleccionado;
+  }
+
+  public guardar() {
+    this.personas.push(this.persona);
+    console.log("Esta es la persona creada" + this.persona.nombre);
+    console.log("Esta es la persona creada" + this.persona.apellido);
+    console.log("Esta es la persona creada" + this.persona.tipoIdentificacion);
+    console.log("Esta es la persona creada" + this.persona.numeroTelefonico);
+    // console.log("Esta es la persona creada" + this.persona.numeroIdentificacion);
+  }
+
 
 }
